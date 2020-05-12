fc2d14074b292238cb1d4321794f590c2ffc4a81
richyung <61220849+richyung@users.noreply.github.com>
2020-05-11 17:37:02 -0700

    Week 10 classwork

---
+++ Week 10/script.js
@@ -0,0 +1,39 @@
+var header = document.getElementById("docHeader");
+var hColorChangeBtn = document.getElementById("headerColorControl");
+
+var headerColorChange = function(){
+	var redComp = Math.random() * 255;
+  	console.log(redCom);
+	var greenComp = Math.random() * 255;
+	var blueComp = Math.random() * 255;
+
+	header.style.backgroundColor = "rgb(" + redComp + ", " + greenComp + ", " + blueComp + ")";
+	
+}
+
+var quokkaImageSwap = function (){
+	console.log(quokkaImage.alt);
+	
+	if(quokkaImage.alt == "1st image of a qute quokka"){ //the 1st image is showing
+	   quokkaImage.src = "quokka2.jpg";
+	   quokkaImage.alt = "2nd image of a qute quokka"
+	   quokkaButton.innerText = "Show the 1st one";
+	}
+	else {//otherwise
+	   quokkaImage.src = "quokka1.jpg";
+	   quokkaImage.alt = "1st image of a qute quokka"
+	   quokkaButton.innerText = "Show the 2nd one";
+	   
+	}
+	
+	
+}
+
+//just when the JS loads
+headerColorChange();
+//when the button is clicked
+hColorChangeBtn.addEventListener("click",headerColorChange);
+
+quokkaButton.addEventListener("click", swapQuokkaImage);
+	
+})
